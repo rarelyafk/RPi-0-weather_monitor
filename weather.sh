8bd046ee6acd17e5cbddee58d1011889e9187240
Matt Hepburn <dmhepburn@gmail.com>
2022-04-02 14:58:12 -0400

    clr, 15min interval, infinite loop, removed readonly declares, var cleanup in trap func

--- weather.sh
+++ weather.sh
@@ -2,27 +2,32 @@
 
 [[ -f tmp.json ]] && rm tmp.json
 
-curl -s 'https://wttr.in/CHS?0' | awk '(NR > 2) {sub(/\[0m.*$/,"[0m"); print}'
+# infinite loop shorthand
+while :; do
+  clear
+  curl -s 'https://wttr.in/CHS?0' | awk '(NR > 2) {sub(/\[0m.*$/,"[0m"); print}'
 
-curl -s 'https://wttr.in/CHS?format=j1' > tmp.json
-declare -r DESC="$(jq -r '.current_condition[0].weatherDesc[0].value' tmp.json)"
-declare -r TEMP="$(jq -r '.current_condition[0].temp_F' tmp.json)"
-declare -r FEEL="$(jq -r '.current_condition[0].FeelsLikeF' tmp.json)"
-declare -r RAIN="$(jq -r '.current_condition[0].precipInches' tmp.json)"
-declare -r WIND="$(jq -r '.current_condition[0].windspeedMiles' tmp.json)"
-declare -r DAWN="$(jq -r '.weather[0].astronomy[0].sunrise' tmp.json)"
-declare -r DUSK="$(jq -r '.weather[0].astronomy[0].sunset' tmp.json)"
-declare -r AVGT="$(jq -r '.weather[0].avgtempF' tmp.json)"
-declare -r MINT="$(jq -r '.weather[0].mintempF' tmp.json)"
-declare -r MAXT="$(jq -r '.weather[0].maxtempF' tmp.json)"
-echo "$DESC $FEEL°F ($TEMP°F)"
-echo
-echo "($AVGT°F ($MINT°F - $MAXT°F))"
-echo
-echo "sun: $DAWN - $DUSK"
+  curl -s 'https://wttr.in/CHS?format=j1' > tmp.json
 
-cleanup() { rm tmp.json; }
+  DESC="$(jq -r '.current_condition[0].weatherDesc[0].value' tmp.json)"
+  TEMP="$(jq -r '.current_condition[0].temp_F' tmp.json)"
+  FEEL="$(jq -r '.current_condition[0].FeelsLikeF' tmp.json)"
+  RAIN="$(jq -r '.current_condition[0].precipInches' tmp.json)"
+  WIND="$(jq -r '.current_condition[0].windspeedMiles' tmp.json)"
+  DAWN="$(jq -r '.weather[0].astronomy[0].sunrise' tmp.json)"
+  DUSK="$(jq -r '.weather[0].astronomy[0].sunset' tmp.json)"
+  AVGT="$(jq -r '.weather[0].avgtempF' tmp.json)"
+  MINT="$(jq -r '.weather[0].mintempF' tmp.json)"
+  MAXT="$(jq -r '.weather[0].maxtempF' tmp.json)"
 
+  echo "$DESC $FEEL°F ($TEMP°F)"
+  echo "($AVGT°F ($MINT°F - $MAXT°F))"
+  echo "sun: $DAWN - $DUSK"
+  sleep 9000
+done
+
+# cleanup trap
+cleanup() { unset DESC TEMP FEEL RAIN WIND DAWN DUSK AVGT MINT MAXT; rm tmp.json; }
 trap 'cleanup' SIGINT EXIT
 
 #curl -s 'wttr.in/CHS?0' | awk '(NR > 2) {print substr($0,0,30)}'
