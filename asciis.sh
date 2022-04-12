8c8188b2f33e16227cf21e592715cb0d26fcbcd7
Matt Hepburn <dmhepburn@gmail.com>
2022-04-11 21:39:47 -0400

    asciis now uses associative array lookup

--- asciis.sh
+++ asciis.sh
@@ -6,32 +6,31 @@ LG='\e[38;5;226m'
 Y='\e[38;5;226m'
 R='\e[0m'
 
+declare -r -A CONDITIONS=(\
+  ['Sunny']='sun'\
+  ['Clear']='sun'\
+  ['Overcast']='overcast'\
+  ['Partly Cloudy']='partly_cloudy'\
+)
+
 center_txt() { printf '%*s\n' "$(( (34 + "${#1}") / 2))" "$1"; }
 # remove escape characters to calculate true length of string
 
-# 13 cols
+# 13 cols?
+sun() {
+  printf '%b%s%b\n' "$Y" "  \   /   " "$R"
+  printf '%b%s%b\n' "$Y" "   .-.    " "$R"
+  printf '%b%s%b\n' "$Y" "― (   ) ― " "$R"
+  printf '%b%s%b\n' "$Y" "   \`-’   " "$R"
+  printf '%b%s%b\n' "$Y" "  /   \   " "$R"
+}
+
 overcast() {
   printf '%b\n' "$DG"
   printf '%s\n' "      .--.    "
   printf '%s\n' "   .-(    ).  "
   printf '%s\n' "  (___.__)__) "
   printf '%b' "$R"
-}
-
-Clear() {
-  printf '%b%s%b\n' "$Y" "  \   /   " "$R"
-  printf '%b%s%b\n' "$Y" "   .-.    " "$R"
-  printf '%b%s%b\n' "$Y" "― (   ) ― " "$R"
-  printf '%b%s%b\n' "$Y" "   \`-’   " "$R"
-  printf '%b%s%b\n' "$Y" "  /   \   " "$R"
-}
-
-Sunny() {
-  printf '%b%s%b\n' "$Y" "  \   /   " "$R"
-  printf '%b%s%b\n' "$Y" "   .-.    " "$R"
-  printf '%b%s%b\n' "$Y" "― (   ) ― " "$R"
-  printf '%b%s%b\n' "$Y" "   \`-’   " "$R"
-  printf '%b%s%b\n' "$Y" "  /   \   " "$R"
 }
 
 partly_cloudy() {
@@ -41,6 +40,5 @@ partly_cloudy() {
   printf '%b%s%b%s%b\n' "$Y" "   /"     "$MG" "(___(__)   " "$R"
 }
 
-"$1"
-# overcast
-#partly_cloudy
+# lookup condition in CONDITIONS associative array and run function associated
+"${CONDITIONS["${1}"]}"
