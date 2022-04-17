7a35170c18a8ca9e24a79e77a49428d7bdb3e692
Matt Hepburn <dmhepburn@gmail.com>
2022-04-17 12:06:20 -0400

    added center_txt func to take into consideration ANSI color escape sequences, adjusted printf's accordingly

--- asciis.sh
+++ asciis.sh
@@ -15,7 +15,15 @@ declare -r -A CONDITIONS=(\
 )
 #ascii for light_rain???
 
-center_txt() { printf '%*s\n' "$(( (34 + "${#1}") / 2))" "$1"; }
+# center_txt() { printf '%*s\n' "$(( (34 + "${#1}") / 2))" "$1"; }
+center_txt() {
+  local -r orig_len="${#1}"
+  local -r chars="$(echo "$1" | sed 's/\x1B\[[0-9;]*m//g')"
+  local -r len="${#chars}"
+  local -r diff="$(( $orig_len - $len ))"
+  local -r center="$(( ( (34 + "$len") / 2 ) + "$diff" ))"
+  printf '%*s\n' "$center" "$1"
+}
 # remove escape characters to calculate true length of string
 
 # echo '1234567890123456789012345678901234'
@@ -24,26 +32,32 @@ center_txt() { printf '%*s\n' "$(( (34 + "${#1}") / 2))" "$1"; }
 
 # 13 cols?
 sun() {
-  printf '%b%s%b\n' "$Y" "              \   /   " "$R"
-  printf '%b%s%b\n' "$Y" "               .-.    " "$R"
-  printf '%b%s%b\n' "$Y" "            ― (   ) ― " "$R"
-  printf '%b%s%b\n' "$Y" "               \`-’   " "$R"
-  printf '%b%s%b\n' "$Y" "              /   \   " "$R"
+  # echo '1234567890123456789012345678901234'
+  center_txt "$(printf '%b%s%b\n' "$Y" "   \   /   " "$R")"
+  center_txt "$(printf '%b%s%b\n' "$Y" "    .-.    " "$R")"
+  center_txt "$(printf '%b%s%b\n' "$Y" "        ― (   ) ― " "$R")"
+  center_txt "$(printf '%b%s%b\n' "$Y" "       \`-’   " "$R")"
+  center_txt "$(printf '%b%s%b\n' "$Y" "   /   \   " "$R")"
+  # echo '1234567890123456789012345678901234'
 }
 
 overcast() {
-  printf '%b\n' "$DG"
-  printf '%s\n' "      .--.    "
-  printf '%s\n' "   .-(    ).  "
-  printf '%s\n' "  (___.__)__) "
+  # echo '1234567890123456789012345678901234'
+  printf '%b' "$DG"
+  center_txt "$(printf '%s\n' "    .--.   ")"
+  center_txt "$(printf '%s\n' " .-(    ). ")"
+  center_txt "$(printf '%s\n' "(___.__)__)")"
   printf '%b' "$R"
+  # echo '1234567890123456789012345678901234'
 }
 
 partly_cloudy() {
-  printf '%b%s%b\n'     "$Y" "   \  /                    "  "$R"
-  printf '%b%s%b%s%b\n' "$Y" " _ /\"\"" "$MG"  ".-.      "  "$R"
-  printf '%b%s%b%s%b\n' "$Y" "   \_"    "$MG" "(   ).    "  "$R"
-  printf '%b%s%b%s%b\n' "$Y" "   /"     "$MG" "(___(__)   " "$R"
+  # echo '1234567890123456789012345678901234'
+  center_txt "$(printf '%b%s%b\n'     "$Y" "   \  /      "  "$R")"
+  center_txt "$(printf '%b%s%b%s%b\n' "$Y" " _ /\"\"" "$MG"  ".-.    "  "$R")"
+  center_txt "$(printf '%b%s%b%s%b\n' "$Y" "   \_"    "$MG" "(   ).  "  "$R")"
+  center_txt "$(printf '%b%s%b%s%b\n' "$Y" "   /"     "$MG" "(___(__)" "$R")"
+  # echo '1234567890123456789012345678901234'
 }
 
 # lookup condition in CONDITIONS associative array and run function associated
